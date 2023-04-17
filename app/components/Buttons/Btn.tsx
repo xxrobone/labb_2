024201c395036b04545c4dac62b58e3e7f54d401
robonexx <robertwagar@gmail.com>
2023-04-17 10:21:15 +0200

    adding btn and imports

---
+++ app/components/Buttons/Btn.tsx
@@ -0,0 +1,34 @@
+import { DetailedHTMLProps, ButtonHTMLAttributes } from 'react'
+import styled, { StyledComponent } from '@emotion/styled'
+import { Theme } from '@emotion/react';
+
+const Button: StyledComponent<{
+  theme?: Theme | undefined;
+  as?: React.ElementType<any> | undefined;
+}, DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, {}> = styled.button`
+  padding: 32px;
+  background-color: #14ecac;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: white;
+    color: #14ecac;
+  }
+
+  &:active {
+    transform: scale(1.01);
+  }
+`
+
+type Props = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {}
+
+const Btn = (props: Props) => {
+  return (
+      <Button {...props} />
+  )
+}
+
+export default Btn
