61d8c753fb14dff74323cb8b63f5c82e48ba6fc5
robonexx <robertwagar@gmail.com>
2023-04-17 10:44:55 +0200

    adding heading component

---
+++ app/components/heading/Heading.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const H1 = styled.h1`
+  padding: 32px;
+  color: #14ecac;
+  font-size: 48px;
+  border-radius: 4px;
+  font-weight: 800;
+`
+
+const Heading = ({ title }: { title?: string; }) => {
+  return (
+    <H1>{title}</H1>
+  )
+}
+
+export default Heading
