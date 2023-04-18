3b91fa3f5e862363a79bc64bead691338ed72901
robonexx <robertwagar@gmail.com>
2023-04-18 11:30:25 +0200

    rebase, removed messed up component

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,15 @@ import styles from './page.module.css'
 import Btn from './components/buttons/Btn'
 import Heading from './components/heading/Heading'
 import List from './components/list/List'
-import Destroy from './components/destroy/Destroy'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <Destroy />
     <main className={styles.main}>
       <Heading title="A new Cool Heading" />
       <List items={["javascript", "typescript", "react", "nextjs", "git", "emotions"]} render={(item: string) => <div>{item.toLowerCase()}</div>}/>
      <Btn>My Button</Btn>
     </main>
-    </>
   )
 }
